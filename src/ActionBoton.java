11885c98b544df3dff62bfccceae08b9e5caaf26
Aitor Costu <acostumerom01@informatica.iesvalledeljerteplasencia.es>
2017-11-03 21:01:19 +0100

    Cambios Viernes 03-11
    Funciona el quitar los botones y sustituir por el valor en color.

--- src/ActionBoton.java
+++ src/ActionBoton.java
@@ -4,43 +4,47 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 /**
- * Clase que implementa el listener de los botones del Buscaminas.
- * De alguna manera tendrá que poder acceder a la ventana principal.
- * Se puede lograr pasando en el constructor la referencia a la ventana.
- * Recuerda que desde la ventana, se puede acceder a la variable de tipo ControlJuego
+ * Clase que implementa el listener de los botones del Buscaminas. De alguna
+ * manera tendrá que poder acceder a la ventana principal. Se puede lograr
+ * pasando en el constructor la referencia a la ventana. Recuerda que desde la
+ * ventana, se puede acceder a la variable de tipo ControlJuego
+ * 
  * @author jesusredondogarcia
  **
  */
-public class ActionBoton implements ActionListener{
-VentanaPrincipal ventana;
+public class ActionBoton implements ActionListener {
+	VentanaPrincipal ventana;
 	int i;
 	int j;
 
 	public ActionBoton() {
-		//TODO
+		// TODO
 	}
-	
-	public ActionBoton(VentanaPrincipal ventana, int i,int j) {
-		this.ventana=ventana;
-		this.i=i;
-		this.j=j;
+
+	public ActionBoton(VentanaPrincipal ventana, int i, int j) {
+		this.ventana = ventana;
+		this.i = i;
+		this.j = j;
 	}
-	
+
 	/**
-	 *Acción que ocurrirá cuando pulsamos uno de los botones.
+	 * Acción que ocurrirá cuando pulsamos uno de los botones.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (ventana.getJuego().abrirCasilla(i, j)) {
-			if (!ventana.getJuego().esFinJuego()) {
-				ventana.getJuego().getMinasAlrededor(i, j);
-			}else {
-				
-			}
-		}else {
+
+		if (ventana.getJuego().abrirCasilla(i, j)) { // Si la casilla no es una mina, accedes al valor de la casilla
 			
-		}
-		
-	}
+			ventana.mostrarNumMinasAlrededor(i, j); // Quita el bot�n y a�ade la etiqueta de la mina.
+			
+			
+			if (ventana.getJuego().esFinJuego()) {
+				ventana.mostrarFinJuego(false);
+			}
+			}else {
+				ventana.mostrarFinJuego(true);
+			}
+		} 
+	
 
 }
